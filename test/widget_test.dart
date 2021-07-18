176c7ef367ad753dcd8fa6f54157a4a179e383bf
Bart van Wezel <bjpa.wezel@portofrotterdam.com>
2021-07-18 15:22:32 +0200

    update null safety + riverpod versions

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:mockito/mockito.dart';
 import 'package:multiple_category_selection/main.dart';
 
 void main() {
